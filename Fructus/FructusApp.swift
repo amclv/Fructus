44feff99f9612170b2f8e492cfe38858e8f750a9
amclv <toscleveland@gmail.com>
2021-05-11 00:29:08 -0500

    Init Project

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Aaron Cleveland on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
