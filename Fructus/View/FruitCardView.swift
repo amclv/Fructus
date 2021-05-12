5b803c7782cf319db3b741edcc93ce46a9ebe79d
Aaron Cleveland <toscleveland@gmail.com>
2021-05-12 00:35:00 -0500

    Added FruitCardView and StartButtonView

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -9,17 +9,47 @@ import SwiftUI
 
 struct FruitCardView: View {
     // MARK: - Properties -
+     
+    @State private var isAnimating: Bool = false
     
     // MARK: - Body -
     var body: some View {
-        VStack(spacing: 20) {
-            // Fruit: Image
-            Image("blueberry")
-            // Fruit: Title
-            Text("Blueberry")
-            // Fruit: Headline
-            // Button: Start
-        } //: VSTACK
+        ZStack {
+            VStack(spacing: 20) {
+                // Fruit: Image
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                // Fruit: Title
+                Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                // Fruit: Headline
+                Text("Blueberries are sweet, nutritious and widly popular fruit all over the world.")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                // Button: Start
+                StartButtonView()
+            } //: VSTACK
+        } //: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"),
+                                                               Color("ColorBlueberryDark")]),
+                                   startPoint: .top,
+                                   endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 
